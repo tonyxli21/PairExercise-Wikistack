9f7b6d28b193c2cb0d7acb5d1859bb9019e291c7
Sakib Salim <sakibsalim5@gmail.com>
2022-03-10 16:41:43 -0500

    part 2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout');
-
+const { db, Page, User } = require('./models');
 const app = express();
+const Sequelize = require('sequelize');
+
+
 app.use(morgan('dev'));
 
 // Gives information on where to look for the static files
@@ -11,6 +14,12 @@ app.use(express.static(__dirname + "/public"));
 // Parses information from an HTTP response
 app.use(express.urlencoded({ extended: false }));
 
+// Check PSQL connection
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
 // Creates GET request
 app.get('/', (req, res, next) => {
   // res.send("hello world");
@@ -18,6 +27,11 @@ app.get('/', (req, res, next) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const init = async () => {
+  await db.sync( {force:true} );
+  app.listen(PORT, () => {
+    console.log(`Server is listning on port ${PORT}!`)
+  })
+}
+
+init();
